82358e5adb71df216f22bbeddcdc2574135c4602
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-30 23:16:18 -0500

    controllers/transaction_controller.py - list_transactions

--- controllers/transaction_controller.py
+++ controllers/transaction_controller.py
@@ -9,29 +9,31 @@ class TransactionController:
         self.account_controller = account_controller
         self.transactions = self.load_transactions()
 
+    def load_transactions(self):
+        try:
+            with open(TRANSACTIONS_FILE, "r") as f:
+                data = json.load(f)
+                return [Transaction(**txn) for txn in data]
+        except FileNotFoundError:
+            return []
 
-def load_transactions(self):
-    try:
-        with open(TRANSACTIONS_FILE, "r") as f:
-            data = json.load(f)
-            return [Transaction(**txn) for txn in data]
-    except FileNotFoundError:
-        return []
+    def save_transactions(self):
+        with open(TRANSACTIONS_FILE, "w") as f:
+            json.dump([txn.__dict__ for txn in self.transactions], f, indent=2)
 
+    def add_transaction(self, transaction):
+        account = self.account_controller.get_account(transaction.account_id)
+        if not account:
+            raise ValueError("Account not found")
+        if transaction.category.type == "income":
+            account.deposit(transaction.amount)
+        else:
+            account.withdraw(transaction.amount)
+        self.transactions.append(transaction)
+        self.save_transactions()
+        self.account_controller.save_accounts()
 
-def save_transactions(self):
-    with open(TRANSACTIONS_FILE, "w") as f:
-        json.dump([txn.__dict__ for txn in self.transactions], f, indent=2)
-
-
-def add_transaction(self, transaction):
-    account = self.account_controller.get_account(transaction.account_id)
-    if not account:
-        raise ValueError("Account not found")
-    if transaction.category.type == "income":
-        account.deposit(transaction.amount)
-    else:
-        account.withdraw(transaction.amount)
-    self.transactions.append(transaction)
-    self.save_transactions()
-    self.account_controller.save_accounts()
+    def list_transactions(self, account_id=None):
+        if account_id:
+            return [txn for txn in self.transactions if txn.account_id == account_id]
+        return self.transactions
