19e4ffcd7e6ac9f27e5030d50980f5178fb8119f
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-30 23:13:46 -0500

    controllers/transaction_controller.py - load_transactions

--- controllers/transaction_controller.py
+++ controllers/transaction_controller.py
@@ -8,3 +8,12 @@ class TransactionController:
     def __init__(self, account_controller):
         self.account_controller = account_controller
         self.transactions = self.load_transactions()
+
+
+def load_transactions(self):
+    try:
+        with open(TRANSACTIONS_FILE, "r") as f:
+            data = json.load(f)
+            return [Transaction(**txn) for txn in data]
+    except FileNotFoundError:
+        return []
