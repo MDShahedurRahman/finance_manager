daf04f031799007f3fe31703e45965046f0bdb21
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-30 23:10:32 -0500

    controllers/account_controller.py - AccountController.load_accounts

--- controllers/account_controller.py
+++ controllers/account_controller.py
@@ -6,3 +6,12 @@ from config import ACCOUNTS_FILE
 class AccountController:
     def __init__(self):
         self.accounts = self.load_accounts()
+
+
+def load_accounts(self):
+    try:
+        with open(ACCOUNTS_FILE, "r") as f:
+            data = json.load(f)
+            return [Account(**acc) for acc in data]
+    except FileNotFoundError:
+        return []
