48e2f4ef04a935d301dee58128ea0248adabe993
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-30 23:21:29 -0500

    controllers/report_controller.py - total_income

--- controllers/report_controller.py
+++ controllers/report_controller.py
@@ -4,3 +4,7 @@ from collections import defaultdict
 class ReportController:
     def __init__(self, transaction_controller):
         self.txn_ctrl = transaction_controller
+
+
+def total_income(self):
+    return sum(txn.amount for txn in self.txn_ctrl.transactions if txn.category.type == "income")
