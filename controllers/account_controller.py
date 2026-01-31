884a372d1cce6b8b1285eb628359fb09f1b61381
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-30 23:12:13 -0500

    controllers/account_controller.py - AccountController.get_account

--- controllers/account_controller.py
+++ controllers/account_controller.py
@@ -7,21 +7,24 @@ class AccountController:
     def __init__(self):
         self.accounts = self.load_accounts()
 
+    def load_accounts(self):
+        try:
+            with open(ACCOUNTS_FILE, "r") as f:
+                data = json.load(f)
+                return [Account(**acc) for acc in data]
+        except FileNotFoundError:
+            return []
 
-def load_accounts(self):
-    try:
-        with open(ACCOUNTS_FILE, "r") as f:
-            data = json.load(f)
-            return [Account(**acc) for acc in data]
-    except FileNotFoundError:
-        return []
+    def save_accounts(self):
+        with open(ACCOUNTS_FILE, "w") as f:
+            json.dump([acc.__dict__ for acc in self.accounts], f, indent=2)
 
+    def add_account(self, account):
+        self.accounts.append(account)
+        self.save_accounts()
 
-def save_accounts(self):
-    with open(ACCOUNTS_FILE, "w") as f:
-        json.dump([acc.__dict__ for acc in self.accounts], f, indent=2)
-
-
-def add_account(self, account):
-    self.accounts.append(account)
-    self.save_accounts()
+    def get_account(self, account_id):
+        for acc in self.accounts:
+            if acc.account_id == account_id:
+                return acc
+        return None
