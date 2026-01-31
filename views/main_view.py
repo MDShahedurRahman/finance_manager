bd73641aaf6a9b06b852cce82e670b0b4a1a5c4e
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-30 23:27:37 -0500

    views/main_view.py - show_transactions

--- views/main_view.py
+++ views/main_view.py
@@ -3,3 +3,9 @@ class MainView:
         print("Accounts:")
         for acc in accounts:
             print(f"{acc.account_id}: {acc.name} - Balance: {acc.balance}")
+
+    def show_transactions(self, transactions):
+        print("Transactions:")
+        for txn in transactions:
+            print(
+                f"{txn.txn_id}: {txn.account_id} | {txn.category.name} | {txn.amount} | {txn.date} | {txn.description}")
