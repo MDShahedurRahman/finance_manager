71a9db9a12d363d3bdeb4b4123ad7a7b7d42c781
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-30 23:04:27 -0500

    models/account.py - Account.withdraw

--- models/account.py
+++ models/account.py
@@ -6,3 +6,8 @@ class Account:
 
     def deposit(self, amount):
         self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            raise ValueError("Insufficient funds")
+        self.balance -= amount
