e5342306b9190237807f0f5cd95278bf2fbf0963
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-01 22:05:49 -0500

    Add Category.is_income

--- models/category.py
+++ models/category.py
@@ -2,3 +2,6 @@ class Category:
     def __init__(self, name, type_):
         self.name = name
         self.type = type_  # "income" or "expense"
+
+    def is_income(self):
+        return self.type == "income"
